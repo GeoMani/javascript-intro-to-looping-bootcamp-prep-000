9f7e2150da91336539b101852498cce1f2b8da24
George Samuel Igoe <george.igoe@gmail.com>
2018-06-11 01:48:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,16 @@
+function makeArray() {
+  const array = []
+  const t = Math.floor(Math.random() * 10)
+
+  for (let i = 0; i < t; i++) {
+    array.push("I am a strange loop.")
+  }
+
+  return [array, t]
+}
+
+
+
 for (let i = 1; i <= 25; i++) {
   if (i === 1) {
     array = [...array, "I am " + i + " strange loop."];
