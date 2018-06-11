f31b4fb1ce6774e55b34e14bb70c49f00eac8b7f
George Samuel Igoe <george.igoe@gmail.com>
2018-06-11 00:02:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,8 @@
 for (let i = 0; i < 25; i++) {
-  
+  if (i === 1) {
+    array = [...array, "I am " + i + " strange loop."]
+  }
+  else {
+    array = [...array, "I am " + i + " strange loops."]
+  }
 }
