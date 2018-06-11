571845fbf1ecbf4f87d0e6e7f15fd747275e299b
George Samuel Igoe <george.igoe@gmail.com>
2018-06-11 01:53:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,23 +1,12 @@
-function makeArray() {
-  const array = []
-  const t = Math.floor(Math.random() * 10)
-
-  for (let i = 0; i < t; i++) {
-    array.push("I am a strange loop.")
-  }
-
-  return [array, t]
-}
-
 
 function forLoop(array) {
   array = [];
   for (let i = 1; i <= 25; i++) {
     if (i === 1) {
-      array = [...array, "I am " + i + " strange loop."];
+      array.push("I am " + i + " strange loop.");
    }
    else {
-      array = [...array, "I am " + i + " strange loops."];
+      array.push("I am " + i + " strange loops.");
    }
   }
 }
